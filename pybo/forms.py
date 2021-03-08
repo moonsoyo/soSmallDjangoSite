bc64211c8cc556fa7cda08b18aea8c54f56a460c
Soyoung Moon <moonsoyo@usc.edu>
2021-03-08 21:42:18 +0900

    Added question posting and error messages

--- pybo/forms.py
+++ pybo/forms.py
@@ -1,7 +1,19 @@
 from django import forms
-from pybo.models import Question
+from pybo.models import Question, Answer
 
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
         fields = ['subject', 'content']
+        labels = {
+            'subject': 'Title',
+            'content': 'Content',
+        }
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
+            'content': 'Content',
+        }
